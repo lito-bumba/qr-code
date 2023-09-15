369341248494aed698a4fb68ae670244dc608134
= <=>
2023-09-15 17:38:46 -0300

    Removing moko permission

--- shared/src/commonMain/kotlin/com/bumba/qrcode/presentation/NavScreen.kt
+++ shared/src/commonMain/kotlin/com/bumba/qrcode/presentation/NavScreen.kt
@@ -12,11 +12,8 @@ import com.bumba.qrcode.presentation.screen_qr_viewer.QRCodeViewer
 import com.bumba.qrcode.presentation.screen_main.MainScreen
 import com.bumba.qrcode.presentation.screen_main.QRGeneratorViewModel
 import com.bumba.qrcode.presentation.screen_scanner.QRCodeScannerScreen
-import com.bumba.qrcode.presentation.screen_scanner.ScannerViewModel
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
-import dev.icerock.moko.permissions.compose.PermissionsControllerFactory
-import dev.icerock.moko.permissions.compose.rememberPermissionsControllerFactory
 
 sealed interface Screen {
     object MainScreen : Screen
@@ -48,16 +45,11 @@ fun NavScreen(appModule: AppModule) {
         key = Unit,
         factory = viewModelFactory { HistoryViewModel(appModule.qrCodeRepository) }
     )
-    val factory: PermissionsControllerFactory = rememberPermissionsControllerFactory()
-    val scannerViewModel = getViewModel(
-        key = Unit,
-        factory = viewModelFactory { ScannerViewModel(factory.createPermissionsController()) }
-    )
 
     when (screenNavState.value) {
         is Screen.QrCodeViewerScreen -> QRCodeViewer(screenNavState)
         is Screen.HistoryScreen -> HistoryScreen(screenNavState, historyViewModel)
-        is Screen.QRCodeScanner -> QRCodeScannerScreen(screenNavState, scannerViewModel)
+        is Screen.QRCodeScanner -> QRCodeScannerScreen(screenNavState)
         else -> MainScreen(screenNavState, generator)
     }
 }
--- shared/src/commonMain/kotlin/com/bumba/qrcode/presentation/screen_scanner/ScannerScreen.kt
+++ shared/src/commonMain/kotlin/com/bumba/qrcode/presentation/screen_scanner/ScannerScreen.kt
@@ -2,24 +2,15 @@ package com.bumba.qrcode.presentation.screen_scanner
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import com.bumba.qrcode.presentation.Screen
-import dev.icerock.moko.permissions.compose.BindEffect
 
 @Composable
 fun QRCodeScannerScreen(
-    screenNavState: MutableState<Screen>,
-    viewModel: ScannerViewModel
+    screenNavState: MutableState<Screen>
 ) {
-    BindEffect(viewModel.permissionsController)
-
-    LaunchedEffect(Unit) {
-        viewModel.onButtonClick()
-    }
-
-    QRCodeScanner(Modifier.fillMaxSize()){
+    QRCodeScanner(Modifier.fillMaxSize()) {
 
     }
 }
